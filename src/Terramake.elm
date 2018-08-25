ac8e67688ddc011e9775375adccca8843e74b303
Karandi Tamas <tkarandi@gmail.com>
2018-08-25 18:49:26 +0200

    Encode tfvars as properties instead of JSON

--- src/Terramake.elm
+++ src/Terramake.elm
@@ -17,7 +17,7 @@ import Terraform.AWS as AWS
 import Terraform.AWS.EC2 as EC2
 import Terraform.AWS.RDS as RDS
 
-{-| Represents the incoming system args (e.g.: filePath)  
+{-| Represents the incoming system args (e.g.: filePath)
 -}
 type alias Flags =
     {
@@ -59,26 +59,42 @@ exportAsTfvarsWithArgs argsFetcher =
 writeTfvars :  Flags -> Tfvars -> Cmd ()
 writeTfvars flags vars =
   let
-      json = JE.encode 2 <| JE.object <| getEncoders vars
+      lines = encodeTfVars "  " "" <| List.map getEncoder vars
   in
-      Task.attempt (\_ -> ()) (File.write (flags.filePath ++ ".tfvars") json)
+      Task.attempt (\_ -> ()) (File.write (flags.filePath ++ ".tfvars") lines)
 
-getEncoders : Tfvars -> List (String, JE.Value)
-getEncoders vars =
-    List.map getEncoder vars
-
-getEncoder : Tfvar -> (String, JE.Value)
+getEncoder : Tfvar -> TfVarLine
 getEncoder var =
   case var of
-    TfvarObject fieldName objVar -> (fieldName, JE.object [getEncoder objVar])
-    TfvarString fieldName value -> (fieldName, JE.string value)
-    TfvarInt fieldName value -> (fieldName, JE.int value)
-    TfvarRegion fieldName value -> (fieldName, JE.string <| AWS.regionToString value)
-    TfvarInstanceType fieldName value -> (fieldName, JE.string <| EC2.instanceTypeToString value)
-    TfvarDbInstanceType fieldName value -> (fieldName, JE.string <| RDS.dbInstanceTypeToString value)
-    TfvarStorageType fieldName value -> (fieldName, JE.string <| RDS.storageTypeToString value)
+    TfvarObject fieldName objVar -> TfVarLineJson fieldName [getEncoder objVar]
+    TfvarString fieldName value -> TfVarLineString fieldName value
+    TfvarInt fieldName value -> TfVarLineInt fieldName value
+    TfvarRegion fieldName value -> TfVarLineString fieldName <| AWS.regionToString value
+    TfvarInstanceType fieldName value -> TfVarLineString fieldName <|EC2.instanceTypeToString value
+    TfvarDbInstanceType fieldName value -> TfVarLineString fieldName <| RDS.dbInstanceTypeToString value
+    TfvarStorageType fieldName value -> TfVarLineString fieldName <| RDS.storageTypeToString value
 
 
 addTerragruntTfvar : String -> Tfvars -> Tfvars
 addTerragruntTfvar source =
   (::) (TfvarObject "terragrunt" ( TfvarObject "terraform"(TfvarString "source" source)))
+
+
+-- TFvars Encode -------------------------------------------------------------------------------------------------------
+type TfVarLine =
+    TfVarLineString String String
+    | TfVarLineInt String Int
+    | TfVarLineJson String (List TfVarLine)
+
+encodeTfVars : String -> String -> List TfVarLine -> String
+encodeTfVars indent actIndent lines =
+    String.join "\n" <| List.map (encodeTfVar indent actIndent) lines
+
+encodeTfVar : String -> String ->  TfVarLine -> String
+encodeTfVar indent actIndent line =
+  case line of
+    TfVarLineString key value -> actIndent ++ key ++ " = \"" ++ value ++ "\""
+    TfVarLineInt key value    -> actIndent ++ key ++ " = " ++ (toString value)
+    TfVarLineJson key lines   -> actIndent ++ key ++ " = {\n"
+                                  ++ (encodeTfVars indent (indent ++ actIndent) lines) ++ "\n"
+                                  ++ actIndent ++ "}"
